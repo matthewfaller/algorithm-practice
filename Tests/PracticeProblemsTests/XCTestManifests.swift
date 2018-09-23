0b6eb66dd42b17af02776f9606d7e36f4d02f81d
Matthew Faller <matthew.faller@rbc.com>
2018-09-22 22:30:50 -0400

    Added A Command To Generate Tests For Linux

--- Tests/PracticeProblemsTests/XCTestManifests.swift
+++ Tests/PracticeProblemsTests/XCTestManifests.swift
@@ -1,9 +1,42 @@
 import XCTest
 
+extension LinkedListTests {
+    static let __allTests = [
+        ("testAppendAddsElement", testAppendAddsElement),
+        ("testAppendAddsManyElements", testAppendAddsManyElements),
+        ("testAppendIncrementsCount", testAppendIncrementsCount),
+        ("testCanInitializeLinkedList", testCanInitializeLinkedList),
+        ("testCanRemoveFirstElementWhenHead", testCanRemoveFirstElementWhenHead),
+        ("testCanReturnFirstElementWhenRemoved", testCanReturnFirstElementWhenRemoved),
+        ("testClosureIterator", testClosureIterator),
+        ("testFactorialImplementation", testFactorialImplementation),
+        ("testFilteredIterator", testFilteredIterator),
+        ("testLinkedListIterator", testLinkedListIterator),
+        ("testMiddleRemoveStillLinksCorrectly", testMiddleRemoveStillLinksCorrectly),
+        ("testNolongerContainsElementOnceRemoved", testNolongerContainsElementOnceRemoved),
+        ("testRemovesElementFromBack", testRemovesElementFromBack),
+        ("testRemovesElementFromMiddle", testRemovesElementFromMiddle),
+        ("testRootHasCorrectCount", testRootHasCorrectCount),
+        ("testRootHasCorrectData", testRootHasCorrectData),
+        ("testVariadicConstructorHasCorrectCount", testVariadicConstructorHasCorrectCount),
+        ("testVariadicOneArgHasCorrectCount", testVariadicOneArgHasCorrectCount),
+        ("testVariadicZeroArgHasCorrectCount", testVariadicZeroArgHasCorrectCount),
+        ("testVariadicZeroArgWillContainLaterAdditions", testVariadicZeroArgWillContainLaterAdditions),
+        ("testVariadicZeroArgWorksProperly", testVariadicZeroArgWorksProperly),
+    ]
+}
+
+extension PracticeProblemsTests {
+    static let __allTests = [
+        ("testExample", testExample),
+    ]
+}
+
 #if !os(macOS)
-public func allTests() -> [XCTestCaseEntry] {
+public func __allTests() -> [XCTestCaseEntry] {
     return [
-        testCase(PracticeProblemsTests.allTests),
+        testCase(LinkedListTests.__allTests),
+        testCase(PracticeProblemsTests.__allTests),
     ]
 }
 #endif
